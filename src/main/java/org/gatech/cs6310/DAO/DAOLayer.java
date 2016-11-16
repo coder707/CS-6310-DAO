83a57218122ca9266923a7605966d5c2f52a8884
Neil <tweet.devcode@gmail.com>
2016-11-16 14:12:20 -0800

    Initial Version

---
+++ src/main/java/org/gatech/cs6310/DAO/DAOLayer.java
@@ -0,0 +1,16 @@
+package org.gatech.cs6310.dao;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class DAOLayer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
